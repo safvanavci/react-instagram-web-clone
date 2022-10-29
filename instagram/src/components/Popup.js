3706a68fadd2c06e22b1c7781674b7dbae530605
safvanavci <43886857+safvanavci@users.noreply.github.com>
2022-10-29 15:50:22 +0300

    gönderi popup açıldığında kapatmak için icon eklendi

--- instagram/src/components/Popup.js
+++ instagram/src/components/Popup.js
@@ -3,34 +3,38 @@ import HeaderPost from './posts/HeaderPost'
 import CommentPost from './posts/CommentPost'
 import PopupBottom from './popup/PopupBottom'
 import PopupComments from './popup/PopupComments'
+import CrossIcon from './icons/CrossIcon'
 
 
 export default function Popup({ popup, setPopup, liked, setLiked }) {
     return (
         <>
             {popup &&
-
-
-                "sa"
-            }
-            <div className='bg-light-black absolute w-screen h-screen top-0 left-0 z-20 flex justify-center items-center'>
-                <div className='h-[95%] w-3/4 flex bg-black'>
-                    <div className='w-[873px] h-[873px] my-auto'>
-                        <img src={post} />
+                <div className='bg-light-black absolute w-screen h-screen top-0 left-0 z-20 flex justify-center items-center'>
+                    <div className='right-4 top-4 absolute cursor-pointer' onClick={()=> setPopup(false)} >
+                        <CrossIcon/>
                     </div>
-                    <div className=' flex-1 bg-white flex flex-col'>
-                        <HeaderPost />
-                        <div className='flex-1 p-4'>
-                            <PopupComments/>
+                    <div className='h-[95%] w-3/4 flex bg-black'>
+                        <div className='w-[873px] h-[873px] my-auto'>
+                            <img src={post} />
                         </div>
-                        <div className='border-t border-border'>
+                        <div className=' flex-1 bg-white flex flex-col'>
+                            <HeaderPost />
+                            <div className='flex-1 p-4'>
+                                <PopupComments />
+                            </div>
+                            <div className='border-t border-border'>
 
-                            <PopupBottom liked={liked} setLiked={setLiked} />
-                            <CommentPost />
+                                <PopupBottom liked={liked} setLiked={setLiked} />
+                                <CommentPost />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+
+
+            }
+
         </>
     )
 }
---
+++ instagram/src/components/icons/CrossIcon.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CrossIcon() {
+    return (
+        <svg aria-label="Close" color="#ffffff" fill="#ffffff" height="18" role="img" viewBox="0 0 24 24" width="18"><title>Close</title><polyline fill="none" points="20.643 3.357 12 12 3.353 20.647" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3"></polyline><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" x1="20.649" x2="3.354" y1="20.649" y2="3.354"></line></svg>
+    )
+}
